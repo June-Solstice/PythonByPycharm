ef6509d63224b39e6c4bb69b29f3884e259bbbef
KLAPPTL <1747265475@qq.com>
2019-07-04 00:00:34 +0800

    时间设置

---
+++ crawler/throttle.py
@@ -0,0 +1,16 @@
+from urllib import parse
+import time
+
+class Throttle:
+    def __init__(self,delay=5):
+        self.delay=delay
+        self.domains={}
+
+    def wait(self,url):
+        domain=parse.urlsplit(url).netloc
+        last_accessed=self.domains.get(domain)
+        if self.delay >0 and last_accessed:
+            sleep_secs=self.delay-(time.time()-last_accessed)
+            if sleep_secs>0:
+                time.sleep(sleep_secs)
+        self.domains[domain]=time.time()
